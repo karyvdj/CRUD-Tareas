b9a9a3fc95df02a5b18f2733dbef4366f022a0c5
karyvdj <kaina51@hotmail.com>
2017-06-14 14:01:35 -0500

    Version 1, agregar tareas a la tabla

---
+++ js/app.js
@@ -0,0 +1,47 @@
+var api = {
+  url: 'https://lab-api-test.herokuapp.com/tasks/'
+};
+
+var $tasksList = $("#tasks-list");
+
+var cargarPagina = function () {
+  cargarTareas();
+  $("#add-form").submit(agregarTarea);
+};
+
+var cargarTareas = function () {
+  $.getJSON(api.url, function (tareas) {
+    tareas.forEach(crearTarea);
+  });
+}
+
+var crearTarea = function (tarea) {
+  var nombre = tarea.name;
+  var estado = tarea.status[0];
+  // creamos la fila
+  var $tr = $("<tr />");
+  // creamos la celda del nombre
+  var $nombreTd = $("<td />");
+  $nombreTd.text(nombre);
+  // creamos la celda del estado
+  var $estadoTd = $("<td />");
+  $estadoTd.text(estado);
+  // agregamos las celdas a la fila
+  $tr.append($nombreTd);
+  $tr.append($estadoTd);
+  // agregamos filas a la tabla
+  $tasksList.append($tr);
+};
+
+var agregarTarea = function (e) {
+  e.preventDefault();
+  var nombre = $("#nombre-tarea").val();
+  $.post(api.url, {
+    name: nombre
+  }, function (tarea) {
+    crearTarea(tarea);
+    $("#myModal").modal("hide");
+  });
+};
+
+$(document).ready(cargarPagina);
