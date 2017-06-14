56e416ef680678f8c1236283e2202243e0a33e91
karyvdj <kaina51@hotmail.com>
2017-06-14 15:12:27 -0500

    version 1 con platilla en js

--- js/app.js
+++ js/app.js
@@ -3,34 +3,34 @@ var api = {
 };
 
 var $tasksList = $("#tasks-list");
+var $plantillaFinal = "";
 
 var cargarPagina = function () {
   cargarTareas();
   $("#add-form").submit(agregarTarea);
 };
 
+var plantillaTarea = "<tr>"+
+          "<td>__nombre__</td>"+
+          "<td>__estado__</td>"+
+          "<td>"+
+            "<a href='#' data-toggle='modal' data-target='.bs-example-modal-sm'> <span class='glyphicon glyphicon-zoom-in'></span> </a>"+
+            "<a href='#'> <span class='glyphicon glyphicon-pencil'></span> </a>"+
+            "<a href='#'> <span class='glyphicon glyphicon-remove-circle'></span> </a>"+
+          "</td>"+
+        "</tr>";
+
 var cargarTareas = function () {
   $.getJSON(api.url, function (tareas) {
     tareas.forEach(crearTarea);
+    $("#tasks-list").html($plantillaFinal);
   });
-}
+};
+
 
 var crearTarea = function (tarea) {
-  var nombre = tarea.name;
-  var estado = tarea.status[0];
-  // creamos la fila
-  var $tr = $("<tr />");
-  // creamos la celda del nombre
-  var $nombreTd = $("<td />");
-  $nombreTd.text(nombre);
-  // creamos la celda del estado
-  var $estadoTd = $("<td />");
-  $estadoTd.text(estado);
-  // agregamos las celdas a la fila
-  $tr.append($nombreTd);
-  $tr.append($estadoTd);
-  // agregamos filas a la tabla
-  $tasksList.append($tr);
+  $plantillaFinal += plantillaTarea.replace("__nombre__", tarea.name)
+  .replace("__estado__", tarea.status)
 };
 
 var agregarTarea = function (e) {
